e8a135f2592d1bbbc53be7e3fa1883169c8aa98d
Ethan Y <88380314+RinUnderscore@users.noreply.github.com>
2022-12-23 16:29:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,36 @@
+import os
+quit = False
+
+# Manually Convert Functions
+def ls():
+	os.system('dir')
+
+def cd(perm):
+	os.system('cd ' + perm)
+
+# Listening for Commands
+def listeningfunc():
+	# Process Command -Specifics Filter
+	try:
+		cmd = listening.split(" ", 1)[0]
+	except:
+		cmd = listening
+	# Specific Commands
+	if cmd == "ls":
+		ls()
+	elif cmd == "q" or cmd == "quit":
+		quit = True
+	elif cmd == "cd":
+		try:
+			cd(listening.split(" ", 1)[1])
+		except IndexError:
+			cd('')
+	else:
+		print(f'WTL: {cmd}: command not found')
+
+# Main Control Loop
+if __name__ == "__main__":
+	print("Windows to Linux (WTL) [Version 1.0.0]")
+	while not quit:
+		listening = input(f"{os.getcwd()}:$ ")
+		listeningfunc()
